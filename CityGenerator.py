f7f07bc2061adbbf9aef0adaf00a36152fb04ccf
michele <gmvalfre@yahoo.it>
2022-11-28 22:47:52 +0100

    substituted road network generator with voronoi diagrams

--- CityGenerator.py
+++ CityGenerator.py
@@ -1,11 +1,51 @@
-from panda3d.core import PNMImage
+import os
+import random
+import yaml
+from panda3d.core import PNMImage, PNMPainter
 
-def generateCity():
+import numpy as np
+from scipy.spatial import Voronoi
+
+def generateRoadMap(heightMap,n_crossroads):
+    roadMap = PNMImage(heightMap.getReadXSize(),heightMap.getReadYSize())
+    roadMap.fill(255,255,255)
+    painter = PNMPainter(roadMap)
+    crossroads = [] 
+    for c in range(n_crossroads):
+        crossroads += [[random.randrange(0,heightMap.getReadXSize()),
+                       random.randrange(0,heightMap.getReadYSize())]]
+    crossroads = np.array(crossroads)
+    voronoi = Voronoi(crossroads)
+    voronoi_verts = voronoi.vertices
+    for region in voronoi.regions:
+        for i in range(1,len(region)):
+            if (region[i] > -1 and region[i-1] > -1):
+                pt0 = voronoi_verts[region[i-1]]
+                pt1 = voronoi_verts[region[i]]
+                painter.drawLine(pt0[0],pt0[1],pt1[0],pt1[1])
+            
+
+    return roadMap 
+        
+
+def generateCity(cityGeneratorPath,heightMapPath):
     #TODO generate density map
     #TODO call roadmap generator
+
+
+    hmap = PNMImage()
+    hmap.read(heightMapPath)
+
+    roadmap = generateRoadMap(hmap,50)
+    roadmap.write('testrmap.pnm')
+    exit(0)
     
+
+
+    return 0
+
     roadmap = PNMImage()
     roadmap.read('test.png')
     for i in range(roadmap.getReadXSize()):
         for j in range(roadmap.getReadYSize()):
-            if roadmap.getXelVal(i,j)
+            x= roadmap.getXelVal(i,j)
--- TestCityGenerator.py
+++ TestCityGenerator.py
@@ -1,11 +1,52 @@
 from direct.showbase.ShowBase import ShowBase
 
+from Panda3dTerrainGenerator import TerrainGenerator
 import CityGenerator 
 
 class Test(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        CityGenerator.generateCity()
+
+        #Generate terrain
+        shape = (1025,1025)
+
+        #textures should be put in the following order according with
+        #their role(for the time being only 3 textures are supported)
+        texture_paths = [
+                    'Panda3dTerrainGenerator/example_textures/dirt/Ground042_1K_Color.jpg',
+                    'Panda3dTerrainGenerator/example_textures/grass/Grass003_1K_Color.jpg',
+                    'Panda3dTerrainGenerator/example_textures/rock/Rock050_1K_Color.jpg',
+                    'Panda3dTerrainGenerator/example_textures/snow/Snow006_1K_Color.jpg',
+                ]
+
+        #the relative scale factors
+        texture_scale_factors = [
+                    10,
+                    10,
+                    10,
+                    10
+                ]
+
+        #Path to a folder which contains nature objects
+        nature_path = 'Panda3dTerrainGenerator/example_nature'
+
+        TerrainGenerator.generateTerrain(
+                                         'Panda3dTerrainGenerator',
+                                         TerrainGenerator.GENERATORS['FractalCellNoise'],
+                                         [TerrainGenerator.MODIFIERS,TerrainGenerator.MODIFIERS['Smooth']],
+                                         TerrainGenerator.FINALIZERS['Playability'],
+                                         shape,
+                                         texture_paths,
+                                         texture_scale_factors,
+                                         nature_path,
+                                         10,
+                                         "test", 
+                                         force=True
+                                        )
+
+        
+
+        CityGenerator.generateCity('.','test/heightmap.pnm')
 
 
 test = Test()
